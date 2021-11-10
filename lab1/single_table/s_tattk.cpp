aef9a39ebe300d55eedbe4f73afc60fddcf10a2b
Victor725 <1149833549@qq.com>
2021-11-10 20:52:26 +0800

    doing s_tattk

--- lab1/single_table/s_tattk.cpp
+++ lab1/single_table/s_tattk.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 double alpha_map[26]={0};
 int count_of_letters=0;
+unordered_map<char,char> map;
 
 void eval_letters_count(string s){
     for(int i=0;i<s.length();i++){
@@ -32,6 +33,28 @@ void cal_and_show(){
     }
 }
 
+string try_decode(string message,char original,char new_one){
+    map[original]=new_one;
+    string true_message;
+    for(int i=0;i<message.length();i++){
+        if(message[i]==original){
+            true_message.push_back(new_one);
+        }
+        else{
+            true_message.push_back(original);
+        }
+    }
+    return true_message;
+}
+
+void print_map(){
+    cout<<"the map is:"<<endl;
+    for(int i='A';i<='Z';i++){
+        cout<<map[i];
+    }
+    cout<<endl;
+}
+
 int main(){
     string message;
     cout<<"please input a message:"<<endl;
@@ -40,4 +63,19 @@ int main(){
     cout<<count_of_letters<<endl;
     cout<<"----------frequency----------"<<endl;
     cal_and_show();
+    cout<<endl;
+    cout<<"Now,you can start decoding it. Please input your map:"<<endl;
+    char original,new_one;
+    char opt=0;
+    while(1){
+        cin>>original>>new_one;
+        string true_message=try_decode(message,original,new_one);
+        cout<<true_message<<endl;
+        cout<<"is it correct?(y/n):";
+        cin>>opt;
+        if(opt=='y'){
+            print_map();
+            cout<<"the true message is:"<<endl<<true_message<<endl;
+        }
+    }
 }
